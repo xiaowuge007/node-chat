06e4badca02fb12b966388c83cbf28a672727426
xiaowuge007 <lihongqi_elign@163.com>
2019-11-13 16:50:20 +0800

    配置文件修改

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,6 +3,7 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 module.exports = {
+  publicPath: './',
   lintOnSave: process.env.NODE_ENV !== "production",
   css: {
     loaderOptions: {
